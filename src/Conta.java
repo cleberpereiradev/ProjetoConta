5fdae38f01ed935eea36042316401da11d35db20
cleberpereiradev <cleberpereiraeng@gmail.com>
2022-09-16 18:15:54 -0300

    Primeira versao exercicio para aula de tecprog em Java

--- src/Conta.java
+++ src/Conta.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Conta {
+public class Conta {
+    public String conta;
+    public String agencia;
+    public double saldo;
+    public String nomeCliente;
+
+    public String getConta() {
+        return conta;
+    }
+
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+
+
+    public double sacar(double valor){
+        this.saldo -= valor;
+        return this.saldo >= valor ? 1 : 0;
+    }
+
+    public void depositar(double valor){
+        if(valor <= 0){
+            System.out.println("Valor inválido para opção depósito");
+        }else{
+            this.saldo += valor;
+        }
+
+    }
+
+    public void imprimir(){
+        System.out.println("Conta: " + conta);
+        System.out.println("Agência: " + agencia);
+        System.out.println("Saldo R$" + saldo);
+        System.out.println("Nome: " + nomeCliente);
+
+    }
+
 }
--- src/ContaTeste.java
+++ src/ContaTeste.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class ContaTeste {
-}
+import java.util.Scanner;
+public class ContaTeste {
+    static Conta cc = new Conta();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcao = 0;
+
+
+        do{
+            mostrarOpcoes();
+            opcao = Integer.parseInt(sc.nextLine());
+            switch (opcao){
+                case 1:
+                    execSacar();
+                    break;
+                case 2:
+                    execDepositar();
+                    break;
+                case 3:
+                    execConsultar();
+                    break;
+                case 4:
+                    execCadastrar();
+                    break;
+                case 9:
+                    System.out.println("FIM");
+                    break;
+                default:
+                    System.out.println("Opcao invalida");
+            }
+        }while(opcao != 9);
+    }
+
+        public static void execSacar(double valor){
+            System.out.println("Digite valor a ser sacado: ");
+            sc.nextDouble(valor);
+
+            cc.sacar(valor);
+            if(cc.sacar == true){
+                System.out.println("Saque realizado");
+            }else{
+                System.out.println("Saque não realizado");
+            }
+        }
+        public static void execConsultar(){
+            cc.imprimir();
+        }
+        public static void execCadastrar(){
+
+        }
+        public static void execDepositar(double valor){
+            System.out.println("Digite valor a ser depositado: ");
+            sc.nextDouble(valor);
+        }
+
+        public static void mostrarOpcoes(){
+            System.out.println("Operações");
+            System.out.println("1- Sacar");
+            System.out.println("2- Depositar");
+            System.out.println("3- Consultar dados");
+            System.out.println("4- Cadastrar novo cliente");
+            System.out.println("9- Sair");
+            System.out.println("Escolha a operação desejada: ");
+        }
+    }
+
